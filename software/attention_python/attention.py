5345f537333aa4ae43f89d31c570c4068a79ec0e
Ashish Ambati <ashish.ambatii@gmail.com>
2026-07-02 23:31:54 +0530

    Update attention.py

--- software/attention_python/attention.py
+++ software/attention_python/attention.py
@@ -1,1 +1,31 @@
 
+#Task -1
+#Dot Product
+
+import numpy as np
+
+Q = np.array([1, 2, 3])
+K = np.array([4, 5, 6])
+
+dot_product = np.dot(Q, K)
+
+print("Dot Product:", dot_product)
+#OUTPUT:- 32
+
+
+
+
+#Task-2
+#perform Q × Kᵀ
+
+import numpy as np
+
+Q = np.array([[1, 2]])
+K = np.array([[2, 3]])
+
+scores = Q @ K.T
+
+print(scores)
+
+#OUTPUT:- [[8]]
+
